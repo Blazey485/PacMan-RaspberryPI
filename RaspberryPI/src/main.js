64c5c4321a1dfbe8df4c0a08a8a80210e8a7c611
Niksan <niksan@Niksan-sin-MacBook-Air.local>
2026-08-31 12:55:42 +0200

    la på skjerm størrelse for den skjermen vi skal bruke.

--- RaspberryPI/src/main.js
+++ RaspberryPI/src/main.js
@@ -1,2 +1,14 @@
 import "./style.css";
-import phazer from "phaser";
+import Phaser, { Scene } from "phaser";
+
+let gameScene = new Phaser.Scene("Game");
+
+//størrelse på skjermen/spillet
+let config = {
+	type: Phaser.AUTO,
+	width: 1900,
+	height: 950,
+	scene: gameScene
+};
+
+let game = new Phaser.Game(config);
