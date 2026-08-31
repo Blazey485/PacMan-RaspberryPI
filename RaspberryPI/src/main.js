c0d10c8d4fd60a484233d58ecd56f8ae2846050a
Blazey485 <niksandhakal@gmail.com>
2026-08-31 18:48:50 +0200

    added background and folder for the background

--- RaspberryPI/src/main.js
+++ RaspberryPI/src/main.js
@@ -1,13 +1,26 @@
 import "./style.css";
 import Phaser, { Scene } from "phaser";
+import background from "./utils/background.png";
 
 let gameScene = new Phaser.Scene("Game");
+
+gameScene.preload = function () {
+	this.load.image("background", background);
+};
+gameScene.create = function () {
+	this.add
+		.sprite(0, 0, "background")
+		.setOrigin(-0.3, -0.2)
+
+		.setScale(0.2);
+};
 
 //størrelse på skjermen/spillet
 let config = {
 	type: Phaser.AUTO,
 	width: 1240,
 	height: 680,
+	backgroundColor: "2d2d2d",
 
 	scale: {
 		mode: Phaser.Scale.FIT,
