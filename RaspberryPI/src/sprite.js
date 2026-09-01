b74ba3215acdcd917c16885199f825747b1ae66f
adelenothilie2009-tech <adelenothilie2009@gmail.com>
2026-09-01 15:11:03 +0200

    trying to render sprites

--- RaspberryPI/src/sprite.js
+++ RaspberryPI/src/sprite.js
@@ -1,4 +1,14 @@
 import "./style.css";
 import Phaser, { Scene } from "phaser";
-import background from "./utils/background.png";
+import sonic from "./utils/sonicface.png";
+
+
+
+gameScene.preload = function(){
+    this.load.image("sonic", sonicLoad)
+}
+
+gameScene.create = function(){
+    this.add.image(0,400,"sonicLoad").setOrigin(0,0)
+}
 
