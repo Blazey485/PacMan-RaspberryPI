7ca0cb4c39b283d63a39fb099fe74df67fa39d21
Blazey485 <niksandhakal@gmail.com>
2026-09-03 08:36:38 +0200

    adda ting til main og nå må vi splitte sprites

--- RaspberryPI/src/main.js
+++ RaspberryPI/src/main.js
@@ -1,26 +1,61 @@
 import "./style.css";
+
 import Phaser, { Scene } from "phaser";
 import background from "./utils/background.png";
+import eggman from "./utils/EggmanSpriteSheet.png";
+import eggmanJSON from "./utils/EggmanSpriteSheet.json";
 
 let gameScene = new Phaser.Scene("Game");
 
 gameScene.preload = function () {
-	this.load.image("background", background);
+	// this.load.image("background", background);
+	// this.load.atlas("eggmanNPC", eggman, eggmanJSON);
 };
+
+
 gameScene.create = function () {
-	this.add
-		.sprite(0, 0, "background")
-		.setOrigin(-0.3, -0.2)
-
-		.setScale(0.2);
-};
 
 
-//størrelse på skjermen/spillet
+//- den value som er på 2. er for hvor stor selve map-pen er. og 64x64 er hvor store bokser det er inni, så alltid ha den 1216x640 og 64x64 i samma gange tabell. 
+	const grid = this.add.grid (
+	608, 320, // x og y senterkoordinatene av gridden på skjermen 
+   1216, 640, // w og h av hele gridden 
+   38, 32, // cell w og h 
+   0x000000, 1, // fill farge, fill transparency
+   0x0000FF, 1// outline farge, på de strekene
+);
+
+
+
+	// this.add
+	// 	.sprite(0, 0, "background")
+	// 	.setOrigin(-0.3, -0.2)
+
+	// 	.setScale(0.2);
+
+	let player = this.add.sprite(
+		200,
+		200,
+		"eggmanNPC",
+		"eggman"
+	);
+
+	
+};;;
+
+
+
+
+
+
+
+
+
+//størrelse på skjermen/spillet og bascially alt config
 let config = {
 	type: Phaser.AUTO,
-	width: 1240,
-	height: 680,
+	width: 1920,
+	height: 1080,
 	backgroundColor: "#2d2d2d",
 
 	scale: {
@@ -30,17 +65,9 @@ let config = {
 	scene: gameScene
 };
 
-let game = new Phaser.Game(config);
 
-const grid = this.add.grid(
-	385, // x
-	258.5, // y senterkoordinatene av gridden på skjermen
-	770,
-	517, // w og h av hele gridden
-	32,
-	32, // cell w og h
-	0x000000,
-	1, // fill farge, fill transparency
-	0xffffff,
-	0.5 // outline farge, outline transparency tror jeg
-);
+
+
+
+// kjøerer koden:
+let game = new Phaser.Game(config);
