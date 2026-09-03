9bb6ae8c28ee401d1ab440aef93d68da51d4e2de
Blazey485 <niksandhakal@gmail.com>
2026-09-03 10:15:56 +0200

    fixsa masse pusher alt samtigig.

--- RaspberryPI/src/main.js
+++ RaspberryPI/src/main.js
@@ -1,9 +1,8 @@
-import "./style.css";
 
 import Phaser, { Scene } from "phaser";
-import background from "./utils/background.png";
-import eggman from "./utils/EggmanSpriteSheet.png";
-import eggmanJSON from "./utils/EggmanSpriteSheet.json";
+// import background from "./utils/background.png";
+// import eggman from "./utils/EggmanSpriteSheet.png";
+// import eggmanJSON from "./utils/EggmanSpriteSheet.json";
 
 let gameScene = new Phaser.Scene("Game");
 
@@ -12,20 +11,20 @@ gameScene.preload = function () {
 	// this.load.atlas("eggmanNPC", eggman, eggmanJSON);
 };
 
-
 gameScene.create = function () {
-
-
-//- den value som er på 2. er for hvor stor selve map-pen er. og 64x64 er hvor store bokser det er inni, så alltid ha den 1216x640 og 64x64 i samma gange tabell. 
-	const grid = this.add.grid (
-	608, 320, // x og y senterkoordinatene av gridden på skjermen 
-   1216, 640, // w og h av hele gridden 
-   38, 32, // cell w og h 
-   0x000000, 1, // fill farge, fill transparency
-   0x0000FF, 1// outline farge, på de strekene
-);
-
-
+	//- den value som er på 2. er for hvor stor selve map-pen er. og 64x64 er hvor store bokser det er inni, så alltid ha den 1216x640 og 64x64 i samma gange tabell.
+	const grid = this.add.grid(
+		608,
+		320, // x og y senterkoordinatene av gridden på skjermen
+		1216,
+		640, // w og h av hele gridden
+		38,
+		32, // cell w og h
+		0x000000,
+		1, // fill farge, fill transparency
+		0x0000ff,
+		1 // outline farge, på de strekene
+	);
 
 	// this.add
 	// 	.sprite(0, 0, "background")
@@ -34,22 +33,12 @@ gameScene.create = function () {
 	// 	.setScale(0.2);
 
 	let player = this.add.sprite(
-		200,
-		200,
+		608,
+		320,
 		"eggmanNPC",
 		"eggman"
 	);
-
-	
-};;;
-
-
-
-
-
-
-
-
+};
 
 //størrelse på skjermen/spillet og bascially alt config
 let config = {
@@ -64,10 +53,6 @@ let config = {
 	},
 	scene: gameScene
 };
-
-
-
-
 
 // kjøerer koden:
 let game = new Phaser.Game(config);
