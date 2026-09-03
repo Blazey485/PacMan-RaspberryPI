f02ff969fa67f559950b0b4510823d53534ba55e
Blazey485 <niksandhakal@gmail.com>
2026-09-03 18:38:03 +0200

    so i moved every player related thing to a secondary file since the main.js was getting very very cluttered. so i found a stackoverflow where it says how to so i followed it. here is the code 65458003 for the SO question.
    also i made the grid to be centered. and the map was small with 38x32 so i made the entire map/grid bigger so in GameSetting.js line 24 and 25 you can see the new size of the map

--- RaspberryPI/src/main.js
+++ RaspberryPI/src/main.js
@@ -1,57 +1,20 @@
 
 import Phaser, { Scene } from "phaser";
-// import background from "./utils/background.png";
-// import eggman from "./utils/EggmanSpriteSheet.png";
-// import eggmanJSON from "./utils/EggmanSpriteSheet.json";
-
-let gameScene = new Phaser.Scene("Game");
-
-gameScene.preload = function () {
-	// this.load.image("background", background);
-	// this.load.atlas("eggmanNPC", eggman, eggmanJSON);
-};
-
-gameScene.create = function () {
-	//- den value som er på 2. er for hvor stor selve map-pen er. og 64x64 er hvor store bokser det er inni, så alltid ha den 1216x640 og 64x64 i samma gange tabell.
-	const grid = this.add.grid(
-		608,
-		320, // x og y senterkoordinatene av gridden på skjermen
-		1216,
-		640, // w og h av hele gridden
-		38,
-		32, // cell w og h
-		0x000000,
-		1, // fill farge, fill transparency
-		0x0000ff,
-		1 // outline farge, på de strekene
-	);
-
-	// this.add
-	// 	.sprite(0, 0, "background")
-	// 	.setOrigin(-0.3, -0.2)
-
-	// 	.setScale(0.2);
-
-	let player = this.add.sprite(
-		608,
-		320,
-		"eggmanNPC",
-		"eggman"
-	);
-};
+import gameSetting from "./GameSetting.js";
 
 //størrelse på skjermen/spillet og bascially alt config
 let config = {
 	type: Phaser.AUTO,
 	width: 1920,
 	height: 1080,
+	parent: "game",
 	backgroundColor: "#2d2d2d",
 
 	scale: {
 		mode: Phaser.Scale.FIT,
 		autoCenter: Phaser.Scale.CENTER_BOTH
 	},
-	scene: gameScene
+	scene: [gameSetting]
 };
 
 // kjøerer koden:
