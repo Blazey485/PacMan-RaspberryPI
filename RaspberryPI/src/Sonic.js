462cd699b9c3e234b30744bfbd23693c33e05f46
Blazey485 <niksandhakal@gmail.com>
2026-09-05 18:07:08 +0200

    SONIC MOVS YES so i had to change the class to physcis arcade before i had like game object. so i found this : https://phaser.discourse.group/t/setvelocity-vx-vy-not-working-as-expected/8258/2 and i noticed that the comments had a diffrent export so i copied it worked.
    new upt. so the velocity is the direction/angle it goes towards so before i had it at 100, now i made it to 0, i had 100 to test it out.
    
    in addition to those i have to fix the flipX and Y. cus it threw a error, prolly cus the tutorials i have seen is for phaser 3 and not for phaser 4

--- RaspberryPI/src/Sonic.js
+++ RaspberryPI/src/Sonic.js
@@ -1,23 +1,17 @@
-import Phaser from "phaser"
+import Phaser from "phaser";
 
+export default class Sonic extends Phaser.Physics.Arcade.Sprite {
+	constructor(scene, x, y) {
+		super(scene, x, y, "sonicPlayer");
 
+		scene.add.existing(this);
+		scene.physics.add.existing(this);
 
+		this.setScale(1.8);
 
+		this.speed = 200;
 
-
-export default class Sonic extends Phaser.GameObjects.Sprite {
-   constructor(scene, x, y) {
-      super(scene, x, y, "sonicPlayer");
-
-      scene.add.existing(this);
-      scene.physics.add.existing(this);
-      
-      this.setScale(1.8);
-
-      this.speed = 200; 
-
-
-      this.keys = scene.input.keyboard.addKeys({
+		this.keys = scene.input.keyboard.addKeys({
 			up: Phaser.Input.Keyboard.KeyCodes.W,
 			down: Phaser.Input.Keyboard.KeyCodes.S,
 			left: Phaser.Input.Keyboard.KeyCodes.A,
@@ -27,26 +21,31 @@ export default class Sonic extends Phaser.GameObjects.Sprite {
 			arrowLeft: Phaser.Input.Keyboard.KeyCodes.LEFT,
 			arrowRight: Phaser.Input.Keyboard.KeyCodes.RIGHT
 		});
+	}
 
-   }
-
-
-
-   update(){
-      if(this.keys.left.isDown || this.keys.arrowLeft.isDown){
-         this.setVelocity(-this.speed, 100)
-         this.flipX(true);
-      }
-      else if(this.keys.right.isDown || this.keys.arrowRight.isDown){
-         this.setVelocity(this.speed, 100)
-         this.flipX(flase)
-      }
-      else if(this.keys.up.isDown || this.keys.arrowUp.isDown){
-         this.setVelocity(100, -this.speed)
-      }
-      else if(this.keys.down.isDown || this.keys.arrowDown.isDown){
-         this.setVelocity(100, this.speed)
-      }
-   
-   }
+	update() {
+		if (
+			this.keys.left.isDown ||
+			this.keys.arrowLeft.isDown
+		) {
+			this.setVelocity(-this.speed, 0);
+			// this.flipX(true);
+		} else if (
+			this.keys.right.isDown ||
+			this.keys.arrowRight.isDown
+		) {
+			this.setVelocity(this.speed, 0);
+			// this.flipX(flase);
+		} else if (
+			this.keys.up.isDown ||
+			this.keys.arrowUp.isDown
+		) {
+			this.setVelocity(0, -this.speed);
+		} else if (
+			this.keys.down.isDown ||
+			this.keys.arrowDown.isDown
+		) {
+			this.setVelocity(0, this.speed);
+		}
+	}
 }
