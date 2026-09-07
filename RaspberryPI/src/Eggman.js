017c64f86a3041d528a917c4bc415eb8e4cfedd8
Blazey485 <niksandhakal@gmail.com>
2026-09-07 18:44:30 +0200

    fixed eggman he follows sonic aother thing is that i made him animate but he lags so badly, prolly cus there are only 6 frames and the fps is set to 10. maybe we buff it? i found the answer to animate in docs here: https://docs.phaser.io/phaser/concepts/animations#has-animation:~:text=key%20%3A%20Unique%20key%20of%20this%20animation%20data read everything below i also changed name for eggmansprite sheet frame names. cus when i logged the eggmannpc it said cannot find frame 102 so i changed it to 101 - 106 works now

--- RaspberryPI/src/Eggman.js
+++ RaspberryPI/src/Eggman.js
@@ -1,12 +1,42 @@
 import Phaser from "phaser";
 
 export default class Eggman
-	extends Phaser.GameObjects.Sprite
+	extends Phaser.Physics.Arcade.Sprite
 {
-	constructor(scene, x, y) {
+	constructor(scene, x, y, target) {
 		super(scene, x, y, "eggmanNPC");
 
 		scene.add.existing(this);
 		this.setScale(0.97);
+		scene.physics.add.existing(this);
+		this.target = target;
+		this.speed = 175;
+	}
+
+	preload() {
+		this.load.sprite;
+	}
+
+	update(time, delta) {
+		if (!this.target) return;
+
+		const distance = Phaser.Math.Distance.Between(
+			this.x,
+			this.y,
+			this.target.x,
+			this.target.y
+		);
+
+		if (distance > 50) {
+			this.scene.physics.moveToObject(
+				this,
+				this.target,
+				this.speed
+			);
+			this.setFlipX(this.target.x < this.x);
+			this.anims.play("eggman_anime", true);
+		} else {
+			this.body.setVelocity(0, 0);
+		}
 	}
 }
