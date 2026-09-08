826293fe12eca871d4f3a2337e00ae9fa0c7b354
Blazey485 <niksandhakal@gmail.com>
2026-09-08 09:48:23 +0200

    added animate for sonic - doesnt work need fix

--- RaspberryPI/src/GameSetting.js
+++ RaspberryPI/src/GameSetting.js
@@ -17,6 +17,8 @@ export default class GameSetting extends Phaser.Scene {
 
 	preload() {
 		// this.load.image("background", background);
+
+		//hvor vi initiater sprites
 		this.load.atlas("eggmanNPC", eggman, eggmanJSON);
 		this.load.atlas("sonicPlayer", sonicPNG, sonicJSON);
 	}
@@ -29,9 +31,9 @@ export default class GameSetting extends Phaser.Scene {
 			1050, // w og h av hele gridden
 			50,
 			50, // cell w og h
-			0xff00000,
-			1, // fill farge, fill transparency
 			0x000000,
+			1, // fill farge, fill transparency
+			0xffffff,
 			1 // outline farge, på de strekene
 		);
 
@@ -44,12 +46,25 @@ export default class GameSetting extends Phaser.Scene {
 
 		//- SONIC CONFIGS
 		this.player = new Sonic(this, 985, 540);
+		console.log(
+			this.textures.get("sonicPlayer").getFrameNames()
+		);
+
+		this.anims.create({
+			key: "sonic_anime",
+			frames: this.anims.generateFrameNames("sonicPlayer", {
+				prefix: "frame",
+				start: 1,
+				end: 7,
+				zeroPad: 0
+			}),
+			frameRate: 10,
+			repeat: -1
+		});
 
 		//- EGGMAN CONFIGS
 		this.eggman = new Eggman(this, 1900, 692, this.player);
-		console.log(
-			this.textures.get("eggmanNPC").getFrameNames()
-		);
+
 		this.anims.create({
 			key: "eggman_anime",
 			frames: this.anims.generateFrameNames("eggmanNPC", {
