b9ed3625bdcb0cdeb78f0781296c713d9731dc62
Blazey485 <niksandhakal@gmail.com>
2026-09-15 20:57:21 +0200

    so sonic wasnt actually fixed somehow it bypassed the bottom borders but it should be fixed now

--- RaspberryPI/src/Sonic.js
+++ RaspberryPI/src/Sonic.js
@@ -11,7 +11,7 @@ export default class Sonic
 
 		// this.setScale(0.3);
 		this.setScale(1.8);
-		this.body.setSize(48, 52);
+		this.body.setSize(64, 64);
 		this.setCollideWorldBounds(true);
 
 
@@ -44,7 +44,7 @@ export default class Sonic
 			// this.setScale(0.3);
 			this.setScale(1.8);
 			this.setCollideWorldBounds(true);
-			this.body.setSize(48, 52);
+			this.body.setSize(64, 64);
 
 		} else if (
 			this.keys.right.isDown ||
@@ -56,7 +56,7 @@ export default class Sonic
 			// this.setScale(0.3)
 			this.setScale(1.8);
 			this.setCollideWorldBounds(true);
-			this.body.setSize(48, 52);
+			this.body.setSize(64, 64);
 
 		}
 
@@ -69,7 +69,7 @@ export default class Sonic
 			this.anims.play("sonic_PlayerUp", true);
 			this.setScale(0.5, 0.3);
 		this.setCollideWorldBounds(true);
-		this.body.setSize(48, 52);
+		this.body.setSize(64, 64);
 
 		} else if (
 			this.keys.down.isDown ||
@@ -79,7 +79,7 @@ export default class Sonic
 			this.anims.play("sonic_PlayerDown", true);
 			this.setScale(0.5, 0.3);
 			this.setCollideWorldBounds(true);
-this.body.setSize(48, 52);
+this.body.setSize(64, 64);
 		}
 	}
 }
